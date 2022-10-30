f6c3dfa68cc1da826d56fd0b96e4070334b673b8
anushkahebbar <56372418+anushkahebbar@users.noreply.github.com>
2022-10-30 21:47:29 +0530

    Adding visualisation notebooks and further results

--- test_scripts/docker_scripts/runAllIOTestsDocker_sync.sh
+++ test_scripts/docker_scripts/runAllIOTestsDocker_sync.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-echo "Async tests"
+echo "Sync tests"
 for j in seqwr seqrd rndwr rndrd
 do
   echo -e "\tIO Operation: $j"
-  sysbench --num-threads=16 --test=fileio --file-num=20 --file-io-mode=async --file-total-size=16G --file-test-mode=$j --time=30 prepare
-  for i in {1..16..1}
+  sysbench --num-threads=32 --test=fileio --file-num=20 --file-io-mode=sync --file-total-size=16G --file-test-mode=$j --time=30 prepare
+  for i in {1..32..1}
   do
     echo "Number of threads: $i"
-	  sysbench --num-threads="$i" --test=fileio --file-num=20 --file-io-mode=async --file-total-size=16G --file-test-mode=$j --time=30 run >> "results_${i}_${j}_async.txt"
+	  sysbench --num-threads="$i" --test=fileio --file-num=20 --file-io-mode=sync --file-total-size=16G --file-test-mode=$j --time=30 run >> "results_${i}_${j}_sync.txt"
   done
-	sysbench --num-threads=16 --test=fileio --file-num=20 --file-io-mode=async --file-total-size=16G --file-test-mode=$j --time=30 cleanup 
+	sysbench --num-threads=32 --test=fileio --file-num=20 --file-io-mode=sync --file-total-size=16G --file-test-mode=$j --time=30 cleanup 
 done
